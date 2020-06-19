511f4f25fddfea16e7ca5ac5aa24ff5cff85725d
Lukas Kurz <lukas@kurzweb.de>
2020-06-19 13:41:41 +0200

    Projektdateien hinzufügen.

---
+++ SchoolTimer/Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 72.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(0, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(373, 102)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "45 Min"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(0, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(373, 22)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "00:00 Uhr"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopRight
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 10000
+        '
+        'Panel1
+        '
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 124)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(373, 33)
+        Me.Panel1.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button1.Location = New System.Drawing.Point(279, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(94, 30)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Timer starten"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 203)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(373, 30)
+        Me.Panel2.TabIndex = 5
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.TextBox1)
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel3.Location = New System.Drawing.Point(0, 157)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(373, 46)
+        Me.Panel3.TabIndex = 6
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(334, 44)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "5"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Label3
+        '
+        Me.Label3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Label3.Location = New System.Drawing.Point(334, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 46)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Min"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(373, 261)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Timer"
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ SchoolTimer/Form1.vb
@@ -0,0 +1,51 @@
+﻿Public Class Form1
+    Structure Stunde
+        Public start As Integer()
+        Public ende As Integer()
+        Public Sub New(s As Integer(), e As Integer())
+            start = s
+            ende = e
+        End Sub
+    End Structure
+    Dim stunden() As Stunde = New Stunde() {
+        New Stunde(New Integer() {0, 0}, New Integer() {0, 0}),
+        New Stunde(New Integer() {7, 40}, New Integer() {8, 25}),
+        New Stunde(New Integer() {8, 30}, New Integer() {9, 15}),
+        New Stunde(New Integer() {9, 35}, New Integer() {10, 20}),
+        New Stunde(New Integer() {10, 25}, New Integer() {11, 10}),
+        New Stunde(New Integer() {11, 20}, New Integer() {12, 5}),
+        New Stunde(New Integer() {12, 10}, New Integer() {12, 55}),
+        New Stunde(New Integer() {12, 56}, New Integer() {13, 59}),
+        New Stunde(New Integer() {14, 0}, New Integer() {14, 45}),
+        New Stunde(New Integer() {14, 50}, New Integer() {15, 35}),
+        New Stunde(New Integer() {15, 50}, New Integer() {16, 35})
+    }
+    Dim index As Integer
+    Private Sub Timer1_Tick() Handles Timer1.Tick
+        Dim h = Date.Now.TimeOfDay.Hours
+        Dim m = Date.Now.TimeOfDay.Minutes
+        Label2.Text = IntToString(h) + ":" + IntToString(m) + ":" + IntToString(Date.Now.TimeOfDay.Seconds)
+
+    End Sub
+    Function IntToString(x As Integer) As String
+        If x.ToString.Length = 1 Then
+            Return "0" + x.ToString
+        End If
+        Return x.ToString
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1_Tick()
+        Timer2_Tick()
+    End Sub
+    Private Sub Timer2_Tick() Handles Timer2.Tick
+        Dim h = Date.Now.TimeOfDay.Hours
+        Dim m = Date.Now.TimeOfDay.Minutes
+        For i As Integer = 0 To stunden.Count - 1
+            If New TimeSpan(stunden(i).start(0), stunden(i).start(1), 0) <= New TimeSpan(h, m, 0) Then
+                index = i
+            End If
+        Next
+        Dim a = New TimeSpan(stunden(index).ende(0), stunden(index).ende(1), 0) - New TimeSpan(h, m, 0)
+        Label1.Text = IntToString(a.Hours) + ":" + IntToString(Math.Abs(a.Minutes))
+    End Sub
+End Class
